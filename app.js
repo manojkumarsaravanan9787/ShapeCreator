f97ed8e32aefff11b5f4f0387d935c7294277156
MANOJ KUMAR SARAVANAN <manojkumarsaravanan9787@gmail.com>
2025-03-21 14:06:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+let button = document.getElementById("generate");
+var numberInShape = 1;
+var oldValue = 0;
+
+button.onclick = () => {
+    let n = document.getElementById("number").value;
+    const circle = document.getElementById("circle");
+    const square = document.getElementById("square");
+    const rectangle = document.getElementById("rectangle");
+    const triangle = document.getElementById("triangle");
+    const trapezoid = document.getElementById("trapezoid");
+    const box = document.getElementById("box");
+
+n = Number(n) + Number(oldValue);
+
+  for( let j = numberInShape; j<=n; j++)
+  {
+    let shape = document.createElement("div");
+    shape.innerHTML += numberInShape;
+    if(circle.checked)
+        shape.classList.add("circle");
+    else if(square.checked)
+        shape.classList.add("square");
+    else if(rectangle.checked)
+        shape.classList.add("rectangle");
+    else if(triangle.checked)
+        shape.classList.add("triangle");
+    else if(trapezoid.checked)
+        shape.classList.add("trapezoid");
+
+    else
+    document.write("Invalid Input");
+    
+    numberInShape++;
+    box.appendChild(shape);
+    oldValue = document.getElementById("box").lastElementChild.innerHTML;
+  }
+};
